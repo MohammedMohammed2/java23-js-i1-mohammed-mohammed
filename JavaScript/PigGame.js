bfb553c1105bd917d818f9af95eaaf97767e8187
MohammedMohammed2 <mohammed.mohammed1@gritacademy.se>
2024-04-04 17:18:34 +0200

    added extra functions
    counts rounds and total score

--- JavaScript/PigGame.js
+++ JavaScript/PigGame.js
@@ -11,17 +11,40 @@ const dicePTag = document.getElementById('DiceNumber');
 const diceNumber = document.getElementById('Roll');
 diceNumber.addEventListener('click',PigGame);
 
+const scoreNumber = document.getElementById('score');
+
+let roundNumber = document.getElementById('RoundNumber');
+
+
+let totalScore = 0;
+let totalRounds = 0;
+
 function PigGame(event){
+    
+    event.preventDefault();
 
     let diceRoll=Math.ceil(Math.random()*6);
 
     dicePTag.innerHTML=diceRoll;
-   
-    event.preventDefault();
-
-    console.log(diceRoll);
 
 
+    function addedValue(){
+    
+        let score = totalScore + diceRoll;
+        totalScore=score; 
+        totalRounds++;   
+        return score;
+    }
+
+    if(diceRoll!=1){
+        scoreNumber.innerHTML=addedValue(score);
+        roundNumber.innerHTML=totalRounds;
+    }
+    else
+    {
+        scoreNumber.innerHTML=totalScore;
+
+    }
 }
 
 
